7588049b15b7878b7708c198b6f5b6f872f5e4d9
PauloStraforini <paulostraforini@gmail.com>
2025-10-15 20:51:33 -0300

    style(signin): format page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,21 @@
+import { BrainIcon } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
     return (
-        <div>
-            <h1>Auth</h1>
+        <div className="grid min-h-screen grid-cols-2">
+            <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+                <div className="flex items-center gap-3 text-lg text-foreground">
+                    <BrainIcon className="h-7 w-7 text-blue-600" />
+                    <span className="font-semibold">MindFlush</span>
+                </div>
 
-            <div>
+                <footer className="text-sm">
+                    Painel central &copy; MindFLush - {new Date().getFullYear()}
+                </footer>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
                 <Outlet />
             </div>
         </div>
