5456db14cec576182461bf9642ab8ed4d2b9e972
PauloStraforini <paulostraforini@gmail.com>
2025-10-16 10:25:07 -0300

    eat(auth): add field validation to sign-in form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,27 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const singInForm = z.object({
+    email: z.string().email()
+})
+
+type SingInForm = z.infer<typeof singInForm>
+
 export function SignIn() {
+
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SingInForm>()
+
+    async function handleSingIn(data: SingInForm) {
+        console.log(data)
+
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+    }
+
     return (
         <>
             <Helmet title="Entrar no MindFlush" />
@@ -20,13 +37,13 @@ export function SignIn() {
                         </p>
                     </div>
 
-                    <form className="space-y-4">
+                    <form onSubmit={handleSubmit(handleSingIn)} className="space-y-4">
                         <div className="space-y-2">
                             <Label htmlFor="email">E-mail profissional</Label>
-                            <Input id="email" type="email" placeholder="exemplo@mindflush.com" />
+                            <Input id="email" type="email" {...register('email')} placeholder="exemplo@mindflush.com" />
                         </div>
 
-                        <Button className="w-full cursor-pointer hover:bg-blue-700" type="submit">
+                        <Button disabled={isSubmitting} className="w-full cursor-pointer hover:bg-blue-700" type="submit">
                             Entrar
                         </Button>
                     </form>
