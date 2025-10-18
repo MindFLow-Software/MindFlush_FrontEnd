9bb08dce6f869bb71c1f86e1ae55ba455044368a
PauloStraforini <paulostraforini@gmail.com>
2025-10-18 11:02:35 -0300

    feat(pagination): add pagination component

--- src/pages/app/patients/patients.tsx
+++ src/pages/app/patients/patients.tsx
@@ -9,6 +9,7 @@ import {
 } from '@/components/ui/table'
 import { PatitentsTableRow } from './patients-table-row'
 import { PatientsTableFilters } from './patients-table-filters'
+import { Pagination } from '@/components/pagination'
 
 export function Patients() {
     return (
@@ -17,31 +18,34 @@ export function Patients() {
 
             <div className="flex flex-col gap-4">
                 <h1 className="text-3xl font-bold tracking-tight">Pacientes</h1>
-            </div>
-            <div className="space-y-2.5">
-                <PatientsTableFilters />
+                <div className="space-y-2.5">
+                    <PatientsTableFilters />
 
-                <div className="rounded-md border">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[64px]" />
-                                <TableHead className="w-[140px]">CPF</TableHead>
-                                <TableHead className='w-[160px]'>Paciente</TableHead>
-                                <TableHead className="w-[160px]">Data de Nascimento</TableHead>
-                                <TableHead className="w-[140px]">Próxima Sessão</TableHead>
-                                <TableHead className="w-[140px]">Status</TableHead>
-                                <TableHead className="w-[164px]" />
-                                <TableHead className="w-[132px]" />
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {Array.from({ length: 10 }).map((_, i) => {
-                                return <PatitentsTableRow key={i} />
-                            })}
-                        </TableBody>
-                    </Table>
+                    <div className="rounded-md border">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[64px]" />
+                                    <TableHead className="w-[140px]">CPF</TableHead>
+                                    <TableHead className='w-[160px]'>Paciente</TableHead>
+                                    <TableHead className="w-[160px]">Data de Nascimento</TableHead>
+                                    <TableHead className="w-[140px]">Próxima Sessão</TableHead>
+                                    <TableHead className="w-[140px]">Status</TableHead>
+                                    <TableHead className="w-[164px]" />
+                                    <TableHead className="w-[132px]" />
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {Array.from({ length: 10 }).map((_, i) => {
+                                    return <PatitentsTableRow key={i} />
+                                })}
+                            </TableBody>
+                        </Table>
+                    </div>
+
+                    <Pagination pageIndex={0} totalCount={105} perPage={10} />
                 </div>
+
             </div>
         </>
     )
